23e286cbf82cf6014d4e37676a6db22fb82d977b
isouvikdas <souvikdas2412@gmail.com>
2026-08-24 01:57:55 +0530

    feat: add ratelimiter - token bucket

--- docstruct/settings.py
+++ docstruct/settings.py
@@ -148,6 +148,14 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/2",
+    }
+}
+
+
 CELERY_BROKER_URL = 'redis://redis:6379/1'
 CELERY_RESULT_BACKEND = 'redis://redis:6379/1'
 
